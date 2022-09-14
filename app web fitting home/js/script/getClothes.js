bd71a6125b0a9fe4a2d7a77ed926ebf590961db4
VictorTHOMAS94 <victor.thomas@epitech.eu>
2022-09-15 00:23:59 +0200

    display clothes

--- app web fitting home/js/script/getClothes.js
+++ app web fitting home/js/script/getClothes.js
@@ -13,15 +13,15 @@
           body: JSON.stringify({a: 1, b: 'Textual content'})
         });
         const content = await rawResponse.json();
-      
+        var clothes_name = "";
+
         n = (content.length)
         console.log(content);
-        // for(i = 0; i <= (n-1); i++){
-            document.getElementById("name").innerHTML = "<li>"+content[0].name +"</li>"
-            document.getElementById("date").innerHTML = "<li>"+content[0].createdAt +"</li>"
-            document.getElementById("name1").innerHTML = "<li>"+content[1].name +"</li>"
-            document.getElementById("date1").innerHTML = "<li>"+content[1].createdAt +"</li>"
-            document.getElementById("name2").innerHTML = "<li>"+content[2].name +"</li>"
-            document.getElementById("date2").innerHTML = "<li>"+content[2].createdAt +"</li>"
-        // }
+        for(i = 0; i <= (n-1); i++){
+          clothes_name += "<h1>"+content[i].name +"</h1>" + "<p>"+content[i].createdAt +"</p>";
+            // document.getElementById("name").innerHTML = "<p>"+content[i].name +"</p>"
+            // document.getElementById("date").innerHTML = "<p>"+content[i].createdAt +"</p>"
+        }
+        document.querySelector("clothe").innerHTML = clothes_name
       })();
+
