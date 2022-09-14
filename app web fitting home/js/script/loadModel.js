d81c64ce81aa8388bdda4552b93e98366c75ef54
Kerlan <kerlan.plumasseau@epitech.eu>
2022-09-14 23:41:54 +0200

    edit loadModel.js localStorage

--- app web fitting home/js/script/loadModel.js
+++ app web fitting home/js/script/loadModel.js
@@ -4,12 +4,21 @@ const urlParams = new URLSearchParams(queryString);
 let modelName = "woman"; // remplacer par default
 if (urlParams.get('modelName') != null) {
     modelName = urlParams.get('modelName');
+    localStorage.setItem('modelName', modelName);
 }
+
 let topGarmentName;
 if (urlParams.get('topGarmentName') != null) {
     topGarmentName = urlParams.get('topGarmentName');
+    localStorage.setItem('topGarmentName', modelName);
+} else {
+    topGarmentName = localStorage.getItem('topGarmentName');
 }
+
 let downGarmentName;
 if (urlParams.get('downGarmentName') != null) {
     downGarmentName = urlParams.get('downGarmentName');
+    localStorage.setItem('downGarmentName', modelName);
+} else {
+    downGarmentName = localStorage.getItem('downGarmentName');
 }
