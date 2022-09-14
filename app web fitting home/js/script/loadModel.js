9881e4a27fa01f1b5ddfda5d23890db1866c42ee
Kerlan <kerlan.plumasseau@epitech.eu>
2022-09-15 00:03:07 +0200

    edit - loadModel and index.html

--- app web fitting home/js/script/loadModel.js
+++ app web fitting home/js/script/loadModel.js
@@ -5,20 +5,23 @@ let modelName = "woman"; // remplacer par default
 if (urlParams.get('modelName') != null) {
     modelName = urlParams.get('modelName');
     localStorage.setItem('modelName', modelName);
+} else {
+    modelName = localStorage.getItem('modelName');
 }
 
 let topGarmentName;
 if (urlParams.get('topGarmentName') != null) {
     topGarmentName = urlParams.get('topGarmentName');
-    localStorage.setItem('topGarmentName', modelName);
+    localStorage.setItem('topGarmentName', topGarmentName);
 } else {
     topGarmentName = localStorage.getItem('topGarmentName');
+    console.log(topGarmentName);
 }
 
 let downGarmentName;
 if (urlParams.get('downGarmentName') != null) {
     downGarmentName = urlParams.get('downGarmentName');
-    localStorage.setItem('downGarmentName', modelName);
+    localStorage.setItem('downGarmentName', downGarmentName);
 } else {
     downGarmentName = localStorage.getItem('downGarmentName');
 }
