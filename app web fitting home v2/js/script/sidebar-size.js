c066fe7cec08e814364fa2d12dc2e3c106d81ce4
Kerlan <kerlan.plumasseau@epitech.eu>
2022-10-06 15:17:20 +0200

    compress button

--- app web fitting home v2/js/script/sidebar-size.js
+++ app web fitting home v2/js/script/sidebar-size.js
@@ -1,7 +1,15 @@
-function openNav(id) {
-  document.getElementById(id).style.width = "250px";
-}
-
-function closeNav(id) {
-  document.getElementById(id).style.width = "0";
+function resizeNav() {
+  if (document.getElementById('sidebar-details').style.right != "-350px" ) {
+    document.getElementById('sidebar-details').style.right = "-350px";
+    document.getElementById('sidebar-favoris').style.left = "-350px";
+    document.getElementById('screen-size').style.right = "100px";
+    document.getElementById('screen-compress').hidden = false;
+    document.getElementById('screen-expand').hidden = true;
+  } else {
+    document.getElementById('sidebar-details').style.right = "0px";
+    document.getElementById('sidebar-favoris').style.left = "0px";
+    document.getElementById('screen-size').style.right = "350px";
+    document.getElementById('screen-compress').hidden = true;
+    document.getElementById('screen-expand').hidden = false;
+  }
 }
