fad720ee3df1a97c81f55f98b5de32bebeeead29
Kerlan <kerlan.plumasseau@epitech.eu>
2022-10-07 10:15:32 +0200

    pill nav

--- app web fitting home v2/js/script/sidebar-size.js
+++ app web fitting home v2/js/script/sidebar-size.js
@@ -6,7 +6,7 @@ function resizeNav() {
     document.getElementById('screen-compress').hidden = false;
     document.getElementById('screen-expand').hidden = true;
   } else {
-    document.getElementById('sidebar-details').style.right = "0px";
+    document.getElementById('sidebar-details').style.right = "100px";
     document.getElementById('sidebar-favoris').style.left = "0px";
     document.getElementById('screen-size').style.right = "350px";
     document.getElementById('screen-compress').hidden = true;
