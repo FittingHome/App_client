4b36d389c72543b00449951fee70472018a3f438
VictorTHOMAS94 <victor.thomas@epitech.eu>
2022-11-02 22:55:47 +0100

    edit register

---
+++ app web fitting home v2/js/script/avatarSetting.js
@@ -0,0 +1,25 @@
+var age = document.querySelector('#numberAge');
+
+age.addEventListener('input', restrictNumber);
+function restrictNumber (e) {  
+  var newValue = this.value.replace(new RegExp(/[^\d]/,'ig'), "");
+  this.value = newValue;
+}
+var size = document.querySelector('#numberSize');
+
+size.addEventListener('input', restrictNumber);
+function restrictNumber (e) {  
+  var newValue = this.value.replace(new RegExp(/[^\d]/,'ig'), "");
+  this.value = newValue;
+}
+ 
+var weight = document.querySelector('#numberWeight');
+
+weight.addEventListener('input', restrictNumber);
+function restrictNumber (e) {  
+  var newValue = this.value.replace(new RegExp(/[^\d]/,'ig'), "");
+  this.value = newValue;
+}
+ 
+
+ 
